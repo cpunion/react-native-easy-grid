24523fa602b51c29bfcb52edb67fef6804622e79
Li Jie <lijie@applean.cn>
2017-09-29 14:23:33 +0800

    Don't expand cached styles

--- Utils/computeProps.js
+++ Utils/computeProps.js
@@ -1,6 +1,4 @@
 var React = require("react");
-import ReactNativePropRegistry
-  from "react-native/Libraries/Renderer/shims/ReactNativePropRegistry";
 var _ = require("lodash");
 
 module.exports = function(incomingProps, defaultProps) {
@@ -14,36 +12,19 @@ module.exports = function(incomingProps, defaultProps) {
   delete incomingProps.style;
 
   // console.log(defaultProps, incomingProps);
-
-  if (incomingProps) _.merge(computedProps, defaultProps, incomingProps);
-  else computedProps = defaultProps;
-
+  if (incomingProps) {
+    _.assign(computedProps, defaultProps, incomingProps);
+  } else {
+    computedProps = defaultProps;
+  }
   // Pass the merged Style Object instead
   if (incomingPropsStyle) {
-    var computedPropsStyle = {};
-    computedProps.style = {};
-    if (Array.isArray(incomingPropsStyle)) {
-      _.forEach(incomingPropsStyle, style => {
-        if (typeof style == "number") {
-          _.merge(computedPropsStyle, ReactNativePropRegistry.getByID(style));
-        } else {
-          _.merge(computedPropsStyle, style);
-        }
-      });
+    if (computedProps.style) {
+      computedProps.style = [computedProps.style, incomingPropsStyle]
     } else {
-      if (typeof incomingPropsStyle == "number") {
-        computedPropsStyle = ReactNativePropRegistry.getByID(
-          incomingPropsStyle
-        );
-      } else {
-        computedPropsStyle = incomingPropsStyle;
-      }
+      computedProps.style = incomingPropsStyle
     }
-
-    _.merge(computedProps.style, defaultProps.style, computedPropsStyle);
   }
-
-  // console.log("computedProps ", computedProps);
 
   return computedProps;
 };
